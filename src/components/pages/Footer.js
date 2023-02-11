dc83e5b1628690df44f6a3c28c508a6def7bd6c8
Alex-C <alexcook1613@gmail.com>
2023-02-10 23:07:47 -0500

    added Footer.js. Updated Portfolio.js, Resume.js, and PortfolioContainer.

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import {
+// Box,
+// Container,
+// Row,
+// Column,
+// FooterLink,
+// Heading,
+// } from "./FooterStyles";
+
+// const Footer = () => {
+
+export default function Footer() {
+    return (
+        <div>
+            <p>Footer</p>
+            <ul>
+                <li><a href="https://github.com/Alex-C-Codes">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/alex-cook-5408b4ab/">LinkedIn</a></li>
+                <li><a href="https://alexcook.itch.io">itch.io</a></li>
+            </ul>
+        </div>
+    );
+}
