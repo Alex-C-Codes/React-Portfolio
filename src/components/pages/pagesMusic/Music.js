0b49003f157e0cdf9bb8ebebd7986c2c8e574e44
Alex-C <alexcook1613@gmail.com>
2023-03-21 16:49:27 -0400

    updated styling for music tab

--- src/components/pages/pagesMusic/Music.js
+++ src/components/pages/pagesMusic/Music.js
@@ -7,7 +7,7 @@ export default function Music() {
             <div className='container-fluid'>
                 <div className='row'>
                     <div className='col text-center'>
-                        <h3>Video Game Music Pack</h3>
+                        <h3>Music Projects</h3>
                         <div className='d-flex justify-content-center'>
                             <iframe title="itch.io" frameBorder="0" src="https://itch.io/embed/1577689" width="552" height="167"><a href="https://alexcook.itch.io/relaxing-pack">Relaxing &amp; Cozy Music Loop Pack by alexcook</a></iframe>
                         </div>
@@ -26,26 +26,22 @@ export default function Music() {
                 <br></br>
                 <div className='row'>
                     <div className='col text-center'>
+                        <hr></hr><br></br>
                         <h3>Credits</h3>
-                        <hr
+                        {/* <hr
                             style={{
                             background: "#000000",
                             height: "1px",
                             border: "none",
                             }}
-                        />
-                        <h4>Video Games</h4>
+                        /> */}
+
+                        <h5>Video Games</h5>
                         <div className='d-flex justify-content-center'>
                             <iframe title='Adarin Farms' src="https://store.steampowered.com/widget/1827730/" frameborder="0" width="552" height="190"></iframe>
                         </div>
-                        <hr
-                            style={{
-                            background: "#000000",
-                            height: "1px",
-                            border: "none",
-                            }}
-                        />
-                        <h4>YouTube Content</h4>
+
+                        <h5>YouTube Content</h5>
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/6XkYSp6lDxk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/L3PFBNhiwoU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/4HHcwiHgd40" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
