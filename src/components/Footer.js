dea259dd6828abc16b8eaf6caf264a4976dc1bef
Alex-C <alexcook1613@gmail.com>
2023-03-27 13:53:03 -0400

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,20 @@
 import React from "react";
 // import fontawesome from '@fortawesome/fontawesome';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid';
+// import { ListGroup } from 'react-bootstrap';
 
 // fontawesome.library.add(faCheckSquare, faCoffee);
 
 export default function Footer() {
     return (
         <div className='centered'>
+                {/* <ListGroup horizontal>
+                    <ListGroup.Item>This</ListGroup.Item>
+                    <ListGroup.Item>ListGroup</ListGroup.Item>
+                    <ListGroup.Item>renders</ListGroup.Item>
+                    <ListGroup.Item>horizontally!</ListGroup.Item>
+                </ListGroup> */}
             <ul>
                 <li className='footerStyle'>
                     <i className="mr-1 fa fa-github" aria-hidden="true"></i>
@@ -15,15 +22,16 @@ export default function Footer() {
                 </li>
                 <li className='footerStyle'>
                     <i className="mr-1 fa fa-linkedin-square" aria-hidden="true"></i>
-                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alex-cook-5408b4ab/">LinkedIn</a></li>
+                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alex-cook-5408b4ab/">LinkedIn</a>
+                </li>
                 <li className='footerStyle'>
-                    <FontAwesomeIcon icon='coffee' />
+                    {/* <FontAwesomeIcon icon='coffee' /> */}
                     <a target="_blank" rel="noopener noreferrer" href="https://alexcook.itch.io">itch.io</a>
                 </li>
-                <li className='footerStyle'>
-                    {/* <FontAwesomeIcon icon="fa-brands fa-itch-io" /> */}
+                {/* <li className='footerStyle'>
+                    <FontAwesomeIcon icon="fa-brands fa-itch-io" />
                     <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@OgmaSquadChannel/featured">YouTube</a>
-                </li>
+                </li> */}
                 {/* <li className='footerStyle'>
                     <FontAwesomeIcon icon="fa-brands fa-itch-io" />
                     <a target="_blank" rel="noopener noreferrer" href="https://alexcook.itch.io">Email</a>
